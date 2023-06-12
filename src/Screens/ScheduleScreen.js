e645e72e058a7263cf32a215b3b3c49705d40c98
mayP2201 <mayra.naupari@clearminds-it.com>
2023-06-12 10:40:28 -0500

    Disponibilidad de horarios

--- src/Screens/ScheduleScreen.js
+++ src/Screens/ScheduleScreen.js
@@ -1,26 +1,40 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import Calendar from 'react-native-big-calendar'
-
+import Principal from '../components/Principal';
 
 const events = [
-    {
-      title: 'Meeting',
-      start: new Date(2020, 1, 11, 10, 0),
-      end: new Date(2020, 1, 11, 10, 30),
-    },
-    {
-      title: 'Coffee break',
-      start: new Date(2020, 1, 11, 15, 45),
-      end: new Date(2020, 1, 11, 16, 30),
-    },
-  ]
+  {
+    title: 'Disponible',
+    start: new Date(2023, 5, 12, 9, 0),
+    end: new Date(2023, 5, 12, 10, 0),
+  },
+  {
+    title: 'Disponible',
+    start: new Date(2023, 5, 14, 10, 0),
+    end: new Date(2023, 5, 14, 11, 0),
+  },
+  {
+    title: 'Disponible',
+    start: new Date(2023, 5, 15, 10, 0),
+    end: new Date(2023, 5, 15, 11, 0),
+  },
+]
 
 export const Schedule = ({ navigation }) => {
-    return (
-        <View>
-            <Text>Schedule</Text>
-            <Calendar events={events} height={600} />
-        </View>
-    )
+  const [selectedAppointment, setSelectedAppointment] = useState(null);
+
+  const handleAppointmentPress = (event) => {
+    setSelectedAppointment(event);
+    // Realiza cualquier acción adicional que desees al seleccionar una cita
+  };
+  return (
+    <Principal>
+      <View>
+        <Text>Schedule</Text>
+        <Calendar events={events}  height={600}
+        onPressEvent={handleAppointmentPress}/>
+      </View>
+    </Principal>
+  )
 }
